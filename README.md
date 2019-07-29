ba5f4e138e4186abf1c10c158aa64288baedd16f
K <kamiyaowl@gmail.com>
2019-07-30 00:27:40 +0900

    cortex_m_rtで書き換え

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rust-STM32F723E-DISCO
 STM32F723E-DISCO
+
+
+# 参考
+
+https://github.com/stm32-rs/stm32f7xx-hal
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 #![no_std]
 #![no_main]
 
+extern crate panic_halt;
+
+use cortex_m_rt::entry;
 use stm32f7::stm32f7x3;
-use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    // TODO: Show Error Info here
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    main();
-    loop{}
-}
-fn main() {
+#[entry]
+fn main() -> ! {
     let mut peripherals = stm32f7x3::Peripherals::take().unwrap();
     let gpioa = &peripherals.GPIOA;
     gpioa.odr.modify(|_, w| w.odr0().set_bit());
+    loop {}
 }
