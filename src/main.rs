6e0147cf662e035c4673513ff13f13e45c7eb4d7
K <kamiyaowl@gmail.com>
2019-07-30 00:09:37 +0900

    ビルドできた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![no_std]
+#![no_main]
+
+use stm32f7::stm32f7x3;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    // TODO: Show Error Info here
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    main();
+    loop{}
+}
 fn main() {
-    println!("Hello, world!");
+    let mut peripherals = stm32f7x3::Peripherals::take().unwrap();
+    let gpioa = &peripherals.GPIOA;
+    gpioa.odr.modify(|_, w| w.odr0().set_bit());
 }
