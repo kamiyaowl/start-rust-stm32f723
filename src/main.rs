0dbf7cc47d3077af44d5cc83d5d3761f564f9ddf
K <kamiyaowl@gmail.com>
2019-08-04 22:56:40 +0900

    cortex-m-quickstartで作り直し

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
+// pick a panicking behavior
+extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// extern crate panic_abort; // requires nightly
+// extern crate panic_itm; // logs messages over ITM; requires ITM support
+// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
+use cortex_m::asm;
 use cortex_m_rt::entry;
-use stm32f7::stm32f7x3;
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = stm32f7x3::Peripherals::take().unwrap();
-    let gpioa = &peripherals.GPIOA;
-    gpioa.odr.modify(|_, w| w.odr0().set_bit());
-    loop {}
+    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    loop {
+        // your code goes here
+    }
 }
